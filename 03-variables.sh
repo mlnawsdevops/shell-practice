f2e80c87b6945f6321007bcf37a68bc43249f79c
mlnawsdevops <dev@123.email>
2026-04-28 07:23:12 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "${PERSON1}:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello ${PERSON1}. Iam fine. How are you doing?"
+echo "${PERSON1}:: Iam doing good ${PERSON2}. whats going on?"
+echo "${PERSON2}:: I started learning shell script ${PERSON1}"
