af7c900ac80c385f433af013d58c305a4d8a352d
mlnawsdevops <dev@123.email>
2026-04-28 07:17:38 +0530

    conservation

---
+++ 02-conservation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you ?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing ?"
+echo "Ramesh:: Iam doing good Suresh. Whats going on ?"
+echo "Suresh:: I started learning shell script Ramesh"
